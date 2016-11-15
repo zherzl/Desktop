6f9722f9fc3b6416a1643fec688580666aebab17
zherzl111 <zlatko.herzl@outlook.com>
2016-11-15 18:24:37 +0100

    Read and write to same text file

---
+++ ReadWriteSameFile/ReadWriteSameFile/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ReadWriteSameFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter desired path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(136, 26);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(151, 20);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "E:\\Tmp\\Turk";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(313, 26);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start parser";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(36, 79);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 17);
+            this.lblError.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 302);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblError;
+    }
+}
+
---
+++ ReadWriteSameFile/ReadWriteSameFile/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadWriteSameFile
+{
+    public partial class Form1 : Form
+    {
+        public string PathName { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            PathName = txtPath.Text;
+            if (Directory.Exists(PathName))
+            {
+                ProcessDirectory(PathName);
+            }
+        }
+
+
+        // Process all files in the directory passed in, recurse on any directories 
+        // that are found, and process the files they contain.
+        public void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory, "*.map", SearchOption.TopDirectoryOnly);
+
+            foreach (string fileName in fileEntries)
+            {
+                ProcessFile(fileName);
+            }
+
+            // Recurse into subdirectories of this directory.
+            foreach (string subdirectory in Directory.GetDirectories(targetDirectory).Reverse())
+            {
+                try
+                {
+                    ProcessDirectory(subdirectory);
+                }
+                catch (Exception ex)
+                {
+                    lblError.Text = ex.Message;
+                }
+            }
+
+        }
+
+        public void ProcessFile(string path)
+        {
+            StreamReader rdr = new StreamReader(path);
+            string line;
+            List<string> splitLines = new List<string>();
+
+            while ((line = rdr.ReadLine()) != null)
+            {
+                if (line.Contains("#Export"))
+                {
+                    line += "*";
+                }
+                splitLines.Add(line);
+            }
+
+            rdr.Close();
+            WriteFile(path, splitLines);
+        }
+
+        private void WriteFile(string path, List<string> splitLines)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var s in splitLines)
+            {
+                sb.Append(string.Format("{0}\n", s));
+            }
+
+            File.WriteAllText(path, sb.ToString());
+        }
+    }
+}
